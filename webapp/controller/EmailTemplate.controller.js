add7ebd733124f34e36efe08250e5eabcab97db1
UI204830 <ui204830@rwe-ag.com>
2021-08-21 00:19:28 +0200

    created EmailTemplate file as entry file only for deploying template on website

--- webapp/controller/EmailTemplate.controller.js
+++ webapp/controller/EmailTemplate.controller.js
@@ -8,24 +8,10 @@ sap.ui.define([
     return BaseController.extend("meldeappui5.controller.EmailTemplate", {
 
         onInit: function () {
-            const notificationcreateModel = this.getModel("notificationcreateModel");
-            this.byId("emailTemplatePage").setModel(notificationcreateModel);
-            const notificationData = notificationcreateModel.getData();
-            const sUrl = "#" + this.getOwnerComponent().getRouter().getURL("CreateNotification");
-            console.log(sUrl);
-        },
-
-        onBack: function () {
-            var sPreviousHash = History.getInstance().getPreviousHash();
-
-            //The history contains a previous entry
-            if (sPreviousHash !== undefined) {
-                window.history.go(-1);
-            } else {
-                // There is no history!
-                // replace the current hash with page 1 (will not add an history entry)
-                this.getOwnerComponent().getRouter().navTo("CreateNotification", null, true);
-            }
+            const emailTemplateModel = this.getModel("emailTemplateModel");
+            this.byId("emailTemplatePage").setModel(emailTemplateModel);
+            const emailTemplateData = emailTemplateModel.getData();
+            console.log(emailTemplateData);
         }
     });
 });
